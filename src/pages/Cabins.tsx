f2e532f380a39e67f2f71228e2c0933b68bac31f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-07-29 11:03:28 +0300

    fetched cabin data from superbase

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,28 +1,26 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import getCabins from "../services/apiCabins";
-import {useEffect} from "react";
+import CabinTable from "../features/cabins/CabinTable.tsx";
+
 
 function Cabins() {
 
-    const fetchCabins = async() => {
-        getCabins
-    }
-
-
-    useEffect(() => {
-
-       getCabins().then((data) => console.log(data))
-    }, [])
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-        </Row>
 
-      <Row>
-          <CabinTable/>
-      </Row>
+      <>
+
+
+          <Row type="horizontal">
+              <Heading as="h1">All cabins</Heading>
+              <p>Filter / Sort</p>
+          </Row>
+
+          <Row>
+              <CabinTable/>
+          </Row>
+
+      </>
+
   );
 }
 
