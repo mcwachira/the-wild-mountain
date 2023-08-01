59bbe9a80218f4a6c696b396af2301bebb298854
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-08-01 12:14:05 +0300

    Added settings page and fetched basic hotel settings for each cabin

--- src/features/settings/UpdateSettingsForm.tsx
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -1,24 +1,34 @@
-import Form from ', ../.. /ui/Form';
+
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import {useSettings} from "./useSettings.ts";
+import Form from "../../ui/Form.tsx";
+import Spinner from "../../ui/Spinner.tsx";
 
 function UpdateSettingsForm() {
+
+    const {isLoading, settings:{minBookingLength, maxBookingLength, maxGuestPerBooking,breakfastPrice } = {} } = useSettings();
+
+
+
+    if(isLoading)  return <Spinner/>
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+        <Input type='number' defaultValue={minBookingLength} id='min-nights' />
       </FormRow>
       <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+        <Input type='number' defaultValue={maxBookingLength} id='max-nights' />
       </FormRow>
       <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+        <Input type='number' defaultValue={maxGuestPerBooking} id='max-guests' />
       </FormRow>
       <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+        <Input type='number' defaultValue={breakfastPrice} id='breakfast-price' />
       </FormRow>
     </Form>
   );
 }
 
 export default UpdateSettingsForm;
+
---
+++ src/features/settings/useSettings.ts
@@ -0,0 +1,14 @@
+import {getSettings} from "../../services/apiSettings.ts";
+import {useQuery} from "@tanstack/react-query";
+
+export function useSettings() {
+
+
+    const {isLoading, error, data: settings} = useQuery({
+        queryKey: ['settings'],
+        queryFn: getSettings
+    })
+
+return {isLoading, error, settings}
+}
+
