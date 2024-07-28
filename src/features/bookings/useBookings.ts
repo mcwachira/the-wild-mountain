7abd7aa557b1bef733c4daeca6ceea0755e65743
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-07-28 09:49:37 +0300

    Implemented the filter  feature in the booking section

--- src/features/bookings/useBookings.ts
+++ src/features/bookings/useBookings.ts
@@ -1,14 +1,26 @@
 import {useQuery} from "@tanstack/react-query";
-import {getCabins} from "../../services/apiCabins.ts";
 import {getBookings} from "../../services/apiBookings.ts";
+import {useSearchParams} from "react-router-dom";
 
 
 export function useBookings(){
+
+    const [searchParams] = useSearchParams()
+
+    //Filter
+    const filterValue = searchParams.get('status');
+    const filter = !filterValue || filterValue=== "all"
+        ? null
+        :      {field:"status" , value: filterValue};
+        // {field:"totalPrice" , value: 5000, method:"gte"};
+        //
+
+
     const {isLoading, data:bookings, error} =  useQuery({
         //query key should be unique
-        queryKey:['bookings'],
+        queryKey:['bookings', filter],
         //function should return a promise
-        queryFn:getBookings
+        queryFn:() => getBookings({filter})
     })
 
 return {
